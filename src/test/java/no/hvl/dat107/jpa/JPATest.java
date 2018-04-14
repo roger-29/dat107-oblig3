07f60626cbb70c6bd8d161b9989b5916198edbd5
Sondre Gjellestad <sondre.gjellestad@hotmail.com>
2018-04-14 03:16:48 +0200

    :white_check_mark:

---
+++ src/test/java/no/hvl/dat107/jpa/JPATest.java
@@ -0,0 +1,19 @@
+package no.hvl.dat107.jpa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class JPATest {
+
+	@BeforeAll
+	void setup() {
+
+	}
+
+	@Test
+	void test() {
+		System.out.println("This test method should be run");
+		Assertions.assertTrue(true);
+	}
+}
